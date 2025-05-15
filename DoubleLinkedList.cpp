2f18c10a49da60fb26e7462373e223c9549a7522
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-15 15:46:19 +0700

    Menambahkan Step 9c dan 9d

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -76,6 +76,13 @@ public:
         //Step 9: Insert between current and current->next
         newNode->next = current->next; // Step 9a : newNode.next = current.next
         newNode->prev = current;       // Step 9b : newNode.prev = current
+
+        //insert last node
+        if (current->next != NULL)
+        current->next->prev=newNode; //Step 9c: current.next.prev
+        current->next=newNode; //Step 9d: current.next = newNode
+      
+        
         
         
             
