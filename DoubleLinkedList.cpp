ae015d3d213990846705784cc2a84db98d6787f0
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-05-15 16:12:21 +0700

    Menambahkan Step 3, Step 4, dan Step 5

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -121,8 +121,17 @@ public:
         }
         else
         {
-            
+            //Step 3: Link previous node to the next of current
+            current->prev->next= current->next;
+
+            //Step 4: If current is not the last node
+            if (current->next != NULL)
+                current->next->prev=current->prev; 
         }
+
+        //Step 5: Delete the node
+        delete current;
+        cout <<"Record with roll number"<<rollNo<<"deleted"<<endl;
         
         
         
